d3adcb0b6c9adfed52c0bd6c4d6a04dbd1b2f424
Dara Oladapo <daraoladapo@outlook.com>
2021-04-26 15:37:36 +0100

    MVC - Routing and Models View

---
+++ PeopleWeb/Controllers/PeopleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWeb.Controllers
+{
+    public class PeopleController : Controller
+    {
+        [Route("every-person-in-the-org")]
+        public IActionResult Index()
+        {
+            List<Person> People = new List<Person>()
+            {
+                new Person{Name="Smith A", EmailAddress="smith@anderson.com", Thumbnail="https://usercontent.one/wp/daraoladapo.com/wp-content/uploads/2020/07/NewProPic.jpg"},
+                new Person{Name="James A", EmailAddress="james@anderson.com", Thumbnail="https://usercontent.one/wp/daraoladapo.com/wp-content/uploads/2020/07/NewProPic.jpg"},
+                new Person{Name="Elizabeth A", EmailAddress="lizzy@anderson.com", Thumbnail="https://usercontent.one/wp/daraoladapo.com/wp-content/uploads/2020/07/NewProPic.jpg"},
+                new Person{Name="Jones A", EmailAddress="jones@anderson.com", Thumbnail="https://usercontent.one/wp/daraoladapo.com/wp-content/uploads/2020/07/NewProPic.jpg"}
+            };
+            return View(People);
+        }
+        [Route("details-of-person")]
+        public IActionResult Details()
+        {
+            var person = new Person { Name = "Smith A", EmailAddress = "smith@anderson.com", Thumbnail = "https://usercontent.one/wp/daraoladapo.com/wp-content/uploads/2020/07/NewProPic.jpg" };
+            return View(person);
+
+        }
+    }
+}
---
+++ PeopleWeb/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleWeb.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
